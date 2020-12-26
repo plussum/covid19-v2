bf42cdc93ebb17b1537368dccd2323c087a94e45
plussum <pls_git@outlook.com>
2020-12-27 08:30:35 +0900

    Re try to parameter format

--- TestCov.pl
+++ TestCov.pl
@@ -6,15 +6,24 @@
 #	-params  : set PARASMS{$_} = 1;
 #	--params : set PARASMS{$_} = $ARGV[+1];
 #	
-#	-create_table : Create Tables listed on @MASTER_DEFS and @RECORD_DEFS
-#	-buid_master  : Insert Data and build @MASTER_DEFS
-#	-delete_data  : Delete all datas from @MASTER_DEFS and @RECORD_DEFS
-#	-load_master  : Load Master (This may default, not yet)
-#	-reform_csv   : Reform data srouce format fit to this program
-#	-insert_csv	  : load (reformed ) csv to @RECORD_DEFS
+#	table_name  	: Select Table_name for operate, Default all tables
 #
+#	-table_info		: Display Table information
+#	-dump		 	: Simple Dump of the table
+#
+#	-create_table	: Create Tables listed on @MASTER_DEFS and @RECORD_DEFS
+#	-buid_master	: Insert Data and build @MASTER_DEFS
+#	-delete_data	: Delete all datas from @MASTER_DEFS and @RECORD_DEFS
+#	-load_master	: Load Master (This may default, not yet)
+#	-reform_csv		: Reform data srouce format fit to this program
+#	-insert_csv		: load (reformed ) csv to @RECORD_DEFS
+#
+#	Examples
 #	./TestCov.pl -create_table -build_master -reform_csv -insert_csv
-#	./TestCov.pl --reform_csv CCSE-NC
+#	./TestCov.pl CCSE-NC -reform_csv 
+#	./TestCov.pl AreaInfo -table_info
+#	./TestCov.pl DataSource -table_info --debug 2
+#	./TestCov.pl AreaInfo -dump 
 #
 #
 use	strict;
@@ -44,24 +53,57 @@ my %AreaInfo = ();
 my %DataSource = ();
 my %DataKind = ();
 my @MASTER_DEFS  = (
-	{table_name => "AreaInfo",   hash => \%AreaInfo, csvf => "$config::WIN_PATH/pop.csv",
-			columns => "AreaID PRIMARY KEY INTEGER, AreaName VARCHAR(64), Population INTEGER", }, 
-	{table_name => "DataSource", hash => \%DataSource, csvf => "DataSource.csv",
+	{table_name => "AreaInfo",   hash => {}, csvf => "$config::WIN_PATH/pop.csv",
+			columns => "AreaID INTEGER PRIMARY KEY, AreaName VARCHAR(64), Population INTEGER", }, 
+	{table_name => "DataSource", hash => {}, csvf => "DataSource.csv",
 			columns => "SourceID INTEGER, SourceName CHAR(8), SoruceFullName VARCHAR(256)", },
-	{table_name => "DataKind",   hash => \%DataKind, csvf => "DataKind.csv",
+	{table_name => "DataKind",   hash => {}, csvf => "DataKind.csv",
 			columns => "KindID INTEGER, KindName CHAR(32), KindFullName VARCHAR(256)", },
 );
+
+#
+#	Set array number of Master Tables and set AreaInfo
+#
+my %MASTER_INDEX = ();
+for(my $i = 0; $i <= $#MASTER_DEFS; $i++){
+	my $p = $MASTER_DEFS[$i];
+	my $name = $p->{table_name};
+	$MASTER_INDEX{$name} = $i;
+}
+
+my $areainfo = $MASTER_INDEX{AreaInfo};
+my $AreaName = $MASTER_DEFS[$areainfo]->{hash};
+
 my @RECORD_DEFS = (
-	{record_name => "CCSE-NC", table_name => "RawRecord", csvf => "testdb.csv", csv_src => "$config::CSV_PATH/time_series_covid19_confirmed_global.csv",
-		columns => "RecordNo INTEGER, Date DATE, Source CHAR(8), Kind CHAR(4),AreaID SMALLINT UNSIGNED, Count INTEGER, Avr7 INTEGER",
+	{name => "CCSE-NC", table_name => "RawRecord", csvf => "testdb.csv", csv_src => "$config::CSV_PATH/time_series_covid19_confirmed_global.csv",
+		columns => "RecordNo INTEGER, Date DATE, Source CHAR(8), Kind CHAR(4),AreaID SMALLINT UNSIGNED, Count INTEGER, Avr7 DECIMAL(10,2) " 
+				. ", PRIMARY KEY (RecordNo)",
 		select => "SELECT Date Source Kind  AreaInfo.AreaName Count Avr7 FROM RawRecord, AreaInfo WHERE RawRecird.AreID=AreaInfo.AreaID",
 		source => "ccse", kind => "NC", comvert => \&comv_ccse,
 	},
 );
+my %RECORD_INDEX = ();
+for(my $i = 0; $i <= $#RECORD_DEFS; $i++){
+	my $p = $RECORD_DEFS[$i];
+	my $name = $p->{table_name};
+	$RECORD_INDEX{$name} = $i;
+	#dp::dp "$name, $i\n";
+}
 
 my @QUERY_DATA = (
 	{query => "SELECT * FROM RawRecord WHERE AreaInfo WHERE RawRecird.AreID=AreaInfo.AreName"},
 );
+
+#
+#	Default Paramers of DO
+#
+my %DISP = (
+	default => {disp_result => 1, disp_line_no => 1, disp_query => 0, disp_name => 0},
+	silent  => {disp_result => 0, disp_line_no => 0, disp_query => 0, disp_name => 0},		# no dislay, use result
+	verbose => {disp_result => 1, disp_line_no => 1, disp_query => 1, disp_name => 0},
+	name    => {disp_result => 1, disp_line_no => 0, disp_query => 0, disp_name => 1},
+);
+
 
 #
 #	Command Line Paramerts
@@ -70,7 +112,7 @@ my @QUERY_DATA = (
 #	--params : set PARASMS{$_} = $ARGV[+1];
 #
 #
-my @PARAM_NAMES_LIST = qw(create_table delete_data build_master load_master reform_csv insert_csv select verbose debug);
+my @PARAM_NAMES_LIST = qw(create_table delete_data build_master load_master reform_csv insert_csv dump select verbose debug table_info table_name);
 my %PARAM_NAMES = ();
 foreach my $k (@PARAM_NAMES_LIST){
 	$PARAM_NAMES{$k} = 1;
@@ -102,9 +144,14 @@ for(my $i = 0; $i <= $#ARGV; $i++){
 		}
 		$PARAMS{$_} = "";
 	}
-	if(/ccse/){
-		&comb_ccse();
-		exit 0;
+	else{
+		if(defined $MASTER_INDEX{$_} || defined $RECORD_INDEX{$_}){
+			$PARAMS{table_name} = $_;
+		}
+		else {
+			dp::dp "Unkonwn parameter or table_name: $_\n";
+			exit 1;
+		}
 	}
 	if(/^=/){
 		s/^=//;
@@ -112,31 +159,38 @@ for(my $i = 0; $i <= $#ARGV; $i++){
 	}
 }
 
+#
+#	form Parameters
+#
+if(defined $PARAMS{verbose}){
+	$VERBOSE = $PARAMS{verbose};
+}
+
+if(defined $PARAMS{debug}){
+	$DEBUG = $PARAMS{debug};
+}
+
 dp::dp %PARAMS . "\n" if($DEBUG);
 if((keys %PARAMS) <= 0){
 	print "usage: $0 " . join(" | ", @PARAM_NAMES_LIST) . "\n";
 	exit;
 }
+if(! defined $PARAMS{table_name}){
+	$PARAMS{table_name} = "";
+}
 
 foreach my $k (keys %PARAMS){
-	dp::dp "PARAMS: $k = $PARAMS{$k} \n" if($DEBUG);
+	dp::dp "PARAMS: $k = $PARAMS{$k} \n"if($DEBUG);
 }
 
-if(defined $PARAMS{verbose}){
-	$VERBOSE = $PARAMS{verbose};
-}
-	
-if(defined $PARAMS{debug}){
-	$DEBUG = $PARAMS{debug};
-}
-	
+
 
 #
 #	Connect to Database
 #
 my $dbh = DBI->connect($dsn, $user, $password, {RaiseError => 0, AutoCommit =>0}) || die $DBI::errstr;
 #my $dbh = DBI->connect($dsn, $user, $password ) || die $DBI::errstr;
-&DO($dbh, 0, "USE $DB_NAME");
+&DO($dbh, "USE $DB_NAME", $DISP{silent});
 
 
 #
@@ -145,9 +199,9 @@ my $dbh = DBI->connect($dsn, $user, $password, {RaiseError => 0, AutoCommit =>0}
 my $pname = "delete_data";
 if(defined $PARAMS{$pname}){
 	foreach my $p (@MASTER_DEFS, @RECORD_DEFS){
-		next if($PARAMS{$pname} && $PARAMS{$pname} ne $p->{table_name});
+		next if($PARAMS{table_name} && $PARAMS{table_name} ne $p->{table_name});
 
-		&DO($dbh, 0, qq{DELETE FROM $p->{table_name};});
+		&DO($dbh, qq{DELETE FROM $p->{table_name};});
 	}
 }
 
@@ -155,17 +209,18 @@ if(defined $PARAMS{$pname}){
 #	Create Table
 #
 $pname = "create_table";
-if($PARAMS{$pname}){
-	&DO($dbh,1, "show tables;");
+if(defined $PARAMS{$pname}){
+	dp::dp "## create_table\n";
+	&DO($dbh, "show tables;") if($VERBOSE);
 	foreach my $p (@MASTER_DEFS, @RECORD_DEFS){
-		next if($PARAMS{$pname} && $PARAMS{$pname} ne $p->{table_name});
+		next if($PARAMS{table_name} && $PARAMS{table_name} ne $p->{table_name});
 
-		&DO($dbh, 0, "DROP TABLE IF EXISTS $p->{table_name}");
-		&DO($dbh, 0, "CREATE TABLE $p->{table_name} ($p->{columns})");
-		&DO($dbh, 1, "SHOW TABLES from $DB_NAME;");
-		&DO($dbh, 1, "DESC $p->{table_name};");
+		&DO($dbh, "DROP TABLE IF EXISTS $p->{table_name}", $DISP{verbose});
+		&DO($dbh, "CREATE TABLE $p->{table_name} ($p->{columns})", $DISP{verbose});
+		&DO($dbh, "SHOW TABLES from $DB_NAME;", $DISP{verbose});
+		&DO($dbh, "DESC $p->{table_name};", $DISP{verbose});
 	}
-	$dbh->do("show tables;");
+	&DO($dbh, "show tables;") if($VERBOSE);
 }
 
 #
@@ -196,7 +251,7 @@ $pname = "load_master";
 if(defined $PARAMS{$pname}){
 	dp::dp "load_master\n";
 	foreach my $p (@MASTER_DEFS){
-		next if($PARAMS{$pname} && $PARAMS{$pname} ne $p->{table_name});
+		next if($PARAMS{table_name} && $PARAMS{table_name} ne $p->{table_name});
 
 		dp::dp "load_master; $PARAMS{$pname}, $p->{table_name}\n";
 		&load_master($p);
@@ -211,7 +266,7 @@ $pname = "reform_csv";
 if(defined $PARAMS{reform_csv}){
 	my $target = $PARAMS{reform_csv};
 	foreach my $p (@RECORD_DEFS){
-		next if($PARAMS{$pname} && $PARAMS{$pname} ne $p->{record_name});
+		next if($PARAMS{table_name} && $PARAMS{table_name} ne $p->{table_name});
 
 		$p->{comvert}->($p);
 	}
@@ -225,7 +280,7 @@ if(defined $PARAMS{$pname}){
 	dp::dp "insert_csv\n";
 	&load_master();
 	foreach my $p (@RECORD_DEFS){
-		next if($PARAMS{$pname} && $PARAMS{$pname} ne $p->{table_name});
+		next if($PARAMS{table_name} && $PARAMS{table_name} ne $p->{table_name});
 
 		dp::dp "insert_csv; $PARAMS{$pname}, $p->{table_name}\n";
 		&insert_table($p);
@@ -233,19 +288,32 @@ if(defined $PARAMS{$pname}){
 }
 
 #
-#	Select 
+#	Table Info
 #
-$pname = "select";
+$pname = "table_info";
 if(defined $PARAMS{$pname}){
-	if(! $PARAMS{$pname}){
-		dp::dp "slect need to select table --select RawRecord\n";
-		exit 1;
-	}
-	&load_master();
-	dp::dp "select $PARAMS{$pname}\n" if($DEBUG);
+	dp::dp "table_info\n";
 	foreach my $p (@MASTER_DEFS, @RECORD_DEFS){
-		dp::dp "### select $PARAMS{$pname} : $p->{table_name}\n" if($DEBUG);
-		next if($PARAMS{$pname} && $PARAMS{$pname} ne $p->{table_name});
+		next if($PARAMS{table_name} && $PARAMS{table_name} ne $p->{table_name});
+
+		&table_info($p);
+	}
+}
+
+#
+#	Dump 
+#
+$pname = "dump";
+if(defined $PARAMS{$pname}){
+	#if(! $PARAMS{$pname}){
+	#	dp::dp "slect need to select table --select RawRecord\n";
+	#	exit 1;
+	#}
+	&load_master();
+	dp::dp "select $PARAMS{table_name}\n" if($DEBUG);
+	foreach my $p (@MASTER_DEFS, @RECORD_DEFS){
+		dp::dp "### select $PARAMS{table_name} : $p->{table_name}\n" if($DEBUG);
+		next if($PARAMS{table_name} && $PARAMS{table_name} ne $p->{table_name});
 
 		dp::dp "### select $p->{table_name}\n" if($DEBUG);
 		&load_table($p, {hash => 0, disp => 2});
@@ -259,6 +327,29 @@ $dbh->disconnect();
 exit 0;
 
 ###############################################################
+
+#
+#
+#
+sub	table_info
+{
+	my($p) = @_;
+
+	my $table_name = $p->{table_name};
+	dp::dp "$table_name\n" if($DEBUG);
+
+	my @rows = &DO($dbh, "select TABLE_ROWS from information_schema.tables where table_name = '$table_name';", $DISP{silent});
+
+	print "$table_name ( $rows[0]records )\n";
+	&DO($dbh, "select * from INFORMATION_SCHEMA.TABLES where TABLE_SCHEMA='$DB_NAME' and TABLE_NAME='$table_name'", $DISP{name});
+	print "-" x 20 . "\n";
+	my $sql_str = "desc $p->{table_name}";
+	&DO($dbh, "desc $table_name");
+	print "\n";
+}
+
+
+
 #
 #	Insert values to Table
 #
@@ -336,14 +427,12 @@ sub	comv_ccse
 	my $source_id = $DataSource{$source};
 	my $kind = $p->{kind};
 	my $kind_id = $DataKind{$kind};
-	my $mp = \%AreaInfo;
 
 	my $filed_names = $p->{hash}->{filed_names};
 
 	&load_master();
 	print "-" x 20 . "\n";
-	#print Dumper %AreaInfo;
-	#print Dumper $mp;
+	#print Dumper $AreaName;
 	print "-" x 20 . "\n";
 
 	dp::dp "src: $src\n";
@@ -377,8 +466,8 @@ sub	comv_ccse
 		}
 		my ($area2, $area1, $lat, $long, @count) = split(/,/, $_);
 		my $area_name = ($area2) ? join(";", $area2, $area1) : $area1;
-		my $area_id = $mp->{$area_name}->{AreaID} // -1;
-		dp::dp "[$area_name][" . $mp->{$area_name}->{AreaID} . "]\n";
+		my $area_id = $AreaName->{$area_name}->{AreaID} // -1;
+		dp::dp "[$area_name][" . $AreaName->{$area_name}->{AreaID} . "]\n" if($DEBUG);
 		if($area_id == -1){
 			dp::dp "Error at AreaCode : [$area_name]\n";
 		}
@@ -387,13 +476,13 @@ sub	comv_ccse
 			my $v = $count[$i] // "Nan";
 			my $line = join(",", $dates[$i], $source, $kind, $area_id, $v, 0);
 			print CSV $line . "\n";
-			dp::dp $line . "\n" if($i < 2); # if($SNO < $DISP_LINES) ;
+			dp::dp $line . "\n" if($VERBOSE && $i < 2); # if($SNO < $DISP_LINES) ;
 		}
 	}
 	close(FD);
 	close(CSV);
 
-	#dp::dp Dumper $mp;
+	#dp::dp Dumper $AreaName;
 }
 
 #
@@ -419,6 +508,7 @@ sub	load_table
 {
 	my ($p, $params) = @_;
 
+	dp::dp "# $p->{table_name} . $p->{hash} \n";
 	my $mp = $p->{hash} // "";
 	%{$mp} = () if($mp);		# clear for recall
 
@@ -484,51 +574,8 @@ sub	load_table
 	dp::dp "MAX_LENGTH: " . join(", ", @max_len[0..$numFields]) . "\n" if($VERBOSE || $DEBUG > 2);
 }
 
-
 #
-#	Execute Sql Query
-#
-sub	DO
-{
-	my($dbh, $verbose, $sql_str) = @_;
-	
-	my @result = ();
-	if(!defined $sql_str){
-		my ($package_name, $file_name, $line) = caller;
-		dp::dp "may forgot verbose or sql_str: $file_name #$line\n";
-		exit ;
-	}
-	
-	$verbose = $verbose // "";
-	dp::dp "# " .  $sql_str ."\n" if($verbose);
-#	my $result = $dbh->do($sql_str);
-#	if(! $result){
-#		print $DBI::errstr
-#	}
-
-	my $sth = $dbh->prepare($sql_str);
-	$sth->execute();
-	if(! defined $sth->{'NUM_OF_FIELDS'}){
-		dp::dp "no result\n" if($verbose);
-		return;
-	}
-	my $numFields = $sth->{'NUM_OF_FIELDS'};
-	my $rno = 0;
-	while(my $row = $sth->fetchrow_arrayref()) {
-		my @w = ();
-		for my $i (0..$numFields){
-			push(@w, $$row[$i] // "");
-		}
-		#print "[$numFields] " . join(" " , @w) . "\n" if($verbose);
-		print "  $rno:" . join(" " , @w) . "\n" if($verbose);
-		push(@result, join(" ", @w));
-		$rno++;
-	}
-	return (@result);
-}
-
-#
-#
+#	Get Record Number of the tabel
 #
 sub	record_number
 {
@@ -536,9 +583,65 @@ sub	record_number
 
 	my $sql_str = "select TABLE_ROWS from information_schema.tables where table_name = '$table_name'";
 
-	my @res = &DO($dbh, 0, $sql_str);
+	my @res = &DO($dbh, $sql_str, $DISP{silent});
 	my $row = ($#res < 0) ? 0 : $res[0];
 	#dp::dp "record_namer $table_name : $row \n";
 	return $row;
+}
+
+
+#
+#	Execute Sql Query
+#
+#	default => {disp_result => 1, disp_line_no => 0, disp_query => 0, disp_name => 0},
+#
+sub	DO
+{
+	my($dbh, $sql_str, $disp) = @_;
+	
+	$disp = $disp // $DISP{default};
+
+	if($DEBUG){
+		my ($package_name, $file_name, $line) = caller;
+		dp::dp "Called from: $file_name: $line\n";
+		dp::dp Dumper $disp;
+	}
+
+	dp::dp "# " .  $sql_str ."\n" if($disp->{disp_query} // "");
+	my @result = ();
+	if(!defined $sql_str){
+		my ($package_name, $file_name, $line) = caller;
+		dp::dp "may forgot verbose or sql_str: $file_name #$line\n";
+		exit ;
+	}
+	
+#	my $result = $dbh->do($sql_str);
+#	if(! $result){
+#		print $DBI::errstr
+#	}
+
+	my $sth = $dbh->prepare($sql_str);
+	$sth->execute();
+	if(! defined $sth->{NUM_OF_FIELDS}){
+		dp::dp "no result\n" if($disp->{disp_result} // "");
+		return;
+	}
+	my $numFields = $sth->{NUM_OF_FIELDS} - 1;
+	my $names = $sth->{NAME};
+	print join(" ", @$names) . "\n" if($disp->{disp_name} // "");
+
+	my $rno = 0;
+	while(my $row = $sth->fetchrow_arrayref()) {
+		$rno++;
+		my @w = ();
+		for my $i (0..$numFields){
+			push(@w, $$row[$i] // "");
+		}
+		#print "[$numFields] " . join(" " , @w) . "\n" if($verbose);
+		my $rs = (($disp->{disp_line_no} // "" ) ? "$rno: " : "") . join(" " , @w) . "\n";
+		print $rs if($disp->{disp_result} // "" );
+		push(@result, join(" ", @w));
+	}
+	return (@result);
 }
 
